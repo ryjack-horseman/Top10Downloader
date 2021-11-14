88206f80ade4a99faba3796f87d778c33f5a2513
ryane <ryanejackson97@yahoo.com>
2021-11-14 11:15:13 -0500

    backend v1 done

--- app/src/main/java/com/lp/android/top10downloader/MainActivity.kt
+++ app/src/main/java/com/lp/android/top10downloader/MainActivity.kt
@@ -31,9 +31,10 @@ class MainActivity : AppCompatActivity() {
                 return rssFeed
             }
 
-            override fun onPostExecute(result: String?) {
+            override fun onPostExecute(result: String) {
                 super.onPostExecute(result)
-                Log.d(TAG, "onPostExecute: parameter is $result")
+                val parseApplications = ParseApplications()
+                parseApplications.parse(result)
 
             }
 
--- app/src/main/java/com/lp/android/top10downloader/ParseApplications.kt
+++ app/src/main/java/com/lp/android/top10downloader/ParseApplications.kt
@@ -5,8 +5,9 @@ import org.xmlpull.v1.XmlPullParser
 import org.xmlpull.v1.XmlPullParserFactory
 import java.lang.Exception
 
+private const val TAG = "ParseApplications"
 class ParseApplications {
-    private val TAG = "ParseApplications"
+
     private val applications = ArrayList<FeedEntry>()
 
     fun parse(xmlData: String): Boolean {
@@ -23,7 +24,7 @@ class ParseApplications {
             var eventType = xpp.eventType
             var currentRecord = FeedEntry()
             while(eventType != XmlPullParser.END_DOCUMENT){
-                val tagName = xpp.name.lowercase() //safe call operator here is probably no longer needed with new Kotlin method
+                val tagName = xpp.name?.lowercase()
                 when(eventType){
                     XmlPullParser.START_TAG -> {
                         Log.d(TAG, "parse: Starting tag for $tagName")
@@ -37,6 +38,7 @@ class ParseApplications {
                         Log.d(TAG, "parse: Ending tag for $tagName")
                         if(inEntry){
                             when(tagName){
+                                //tagnames are always lowercase
                                 "entry" -> {
                                     applications.add(currentRecord)
                                     inEntry = false
@@ -54,6 +56,13 @@ class ParseApplications {
                 //get next item
                 eventType = xpp.next()
             }
+
+            //log printout
+            for(app in applications){
+                Log.d(TAG, "**************")
+                Log.d(TAG, app.toString())
+            }
+
         }catch (e: Exception){
             e.printStackTrace()
             status = false
